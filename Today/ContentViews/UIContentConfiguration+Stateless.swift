39a1642174ffbdb911d7d1bacf762eb4788fbc49
Will <williamkerwin@lancsfirerescue.org.uk>
2024-07-08 22:13:06 +0100

    set up editign view

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Will Kerwin on 08/07/2024.
+//
+
+import UIKit
+
+extension UIContentConfiguration{
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
